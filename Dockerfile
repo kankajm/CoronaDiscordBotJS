1bc4c526316ea89ad2e414efbd652ecc2fd81c54
Jaroslav Kaňka <kanka@jkanka.cz>
2020-08-20 23:49:13 +0200

    1.2 DEVELOPMENT
    CHANGELOG:
     - Webserver was rewritten to Express.JS
     - Port was changed from 50001 to 5001
     - Webserver and bot is now working in Docker.

--- Dockerfile
+++ Dockerfile
@@ -8,7 +8,8 @@ RUN npm install discord.js
 RUN npm install axios
 RUN npm install chalk
 RUN npm install is-reachable
+RUN npm install express
 
-EXPOSE 50001
+EXPOSE 5001
 
 CMD node /app/index.js
--- webserver.js
+++ webserver.js
@@ -1,20 +1,16 @@
-const http = require("http");
+const express = require('express');
 const chalk = require('chalk');
-const { checkerPort } = require('./config.json');
-
-const host = 'localhost';
-const port = checkerPort;
-
-const requestListener = function (req, res) {
-    res.writeHead(200);
-    res.end();
-};
 
 function startWebServer() {
-    const server = http.createServer(requestListener);
-    server.listen(port, host, () => {
-    console.log(chalk.green.bold('[INFO]') + ' ONLINE CONTROL IS RUNNING ON PORT: ' + port)
+    const app = express();
+
+    app.get('/', (req, res) => {
+        res.send({"uptime": process.uptime(),
+                  "process_name": "coronabot",
+                  "node_version": process.version});
     });
+    const port = 5001;
+    app.listen(port, () => console.log(chalk.green.bold('[INFO]') + ' ONLINE CONTROL IS RUNNING ON PORT: ' + port));
 }
 
 module.exports = { startWebServer }
