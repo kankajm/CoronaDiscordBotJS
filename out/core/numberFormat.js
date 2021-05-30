ad9b44fdcbe73791399909f9254df3ef4349c3a0
Jaroslav Kaňka <kanka@kankaj.cz>
2021-05-30 22:14:19 +0200

    Delete out directory

--- out/core/numberFormat.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-function formatNumber(num) {
-    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
-}
-module.exports = { formatNumber };
--- out/core/systemInfo.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-const os = require('os');
-const osName = require('os-name');
-module.exports = { systemName: osName(os.platform(), os.release()), nodeVersion: process.version };
